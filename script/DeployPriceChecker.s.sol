de8fba90bb810f233d0bb063462e9fe3e88f98d3
David Brailovsky <davidbrai@gmail.com>
2023-02-14 11:10:21 +0100

    update to wsteth <-> reth

--- script/DeployPriceChecker.s.sol
+++ script/DeployPriceChecker.s.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
-import {RethExpectedOutPriceChecker} from "../src/RethExpectedOutPriceChecker.sol";
+import {WstethRethExpectedOutPriceChecker} from "../src/WstethRethExpectedOutPriceChecker.sol";
 import {DynamicSlippageChecker} from "../src/DynamicSlippageChecker.sol";
 
 contract DeployPriceCheckerScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        new DynamicSlippageChecker("RETH_DYNAMIC_SLIPPAGE_PRICE_CHECKER", address(new RethExpectedOutPriceChecker()));
+        new DynamicSlippageChecker("RETH_DYNAMIC_SLIPPAGE_PRICE_CHECKER", address(new WstethRethExpectedOutPriceChecker()));
 
         vm.stopBroadcast();
     }
